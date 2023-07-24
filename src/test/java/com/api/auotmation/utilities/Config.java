49f3cc3f0052b8e0179891f71aff74fea70c0c04
jass0 <gurbani727@gmail.com>
2023-07-23 20:40:23 -0500

    GET request files for program module added

--- src/test/java/com/api/auotmation/utilities/Config.java
+++ src/test/java/com/api/auotmation/utilities/Config.java
@@ -4,8 +4,17 @@ public class Config {
 	
 	public static final String base_URL="https://lms-api-hackathon-june2023-930a8b0f895d.herokuapp.com/lms";
 	public static final String programMod_POST_URL="https://lms-api-hackathon-june2023-930a8b0f895d.herokuapp.com/lms/saveprogram";		
-	
+	public static final String programMod_GET_URL="https://lms-api-hackathon-june2023-930a8b0f895d.herokuapp.com/lms/allPrograms";  
+
+	public static final String programMod_GET_URL_forexcel="https://lms-api-hackathon-june2023-930a8b0f895d.herokuapp.com/lms/programs";  
 	public static final String excelFilePath="./Data Files/API-Phase2-RESTAssuredCucumber-Data.xlsx";
 	//public static final String excelFilePath="/API.Phase2.RestAssuredCucumber/Data Files/API-Phase2-RESTAssuredCucumber-Data.xlsx";
-
+    
+	
+	public static final String BASE_Invalid_URL = "https://lms-backend.herokuapp.com/lms";
+	public static final String Valid_BatchPut = "/batches/2442";
+	public static final String Valid_BatchPut6495 = "/batches/6495";
+	public static final String Invalid_BatchPut = "/batchessssssss/1456"; //not mine
+	public static final String Invalid_Batch="/batches/6011"; //mine
+	
 }
