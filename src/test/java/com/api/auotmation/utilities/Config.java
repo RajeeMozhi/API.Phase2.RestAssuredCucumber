7b0356c80c941155ae4e97d266a7574698f5e179
Ahila Narendran <Ahila.1985@gmail.com>
2023-07-27 00:46:33 -0400

    Ahila final project updates

--- src/test/java/com/api/auotmation/utilities/Config.java
+++ src/test/java/com/api/auotmation/utilities/Config.java
@@ -8,4 +8,10 @@ public class Config {
 	//public static final String excelFilePath="/API.Phase2.RestAssuredCucumber/Data Files/API-Phase2-RESTAssuredCucumber-Data.xlsx";
 	//public static final String DeleteProgram_URL = "https://lms-backend-service.herokuapp.com/lms/deletebyprogid";
    public static final String BATCHMOD_DELETE_URL =base_URL + "/batches/{batchId}";
+   public static final String USERMOD_DELETE_URL=base_URL+"/users/users/{userId}";
+   public static final String SUBMISSIONMOD_GET_URL=base_URL+"/assignmentsubmission";
+   public static final String SUBMISSIONMOD_GETGRADE_ASSIGNMENTID=base_URL+"/assignmentsubmission/getGrades/{assignmentId}";
+   public static final String SUBMISSIONMOD_GETGRADE_STUDENTID=base_URL+"/assignmentsubmission/getGradesByStudentId/{studentID}";
+   public static final String SUBMISSIONMOD__GETSUBMISSION_BATCHID=base_URL+"/assignmentsubmission/grades/{batchID}";
+   public static final String SUBMISSIONMOD_GETSUBMISSION_USERID=base_URL+"/assignmentsubmission/student/{userId}";
 }
